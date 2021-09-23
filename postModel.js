f8ca295ce79e7651c4f6d87332f3f905baae26b1
Robin Varghese <robin661989@gmail.com>
2021-09-23 21:31:52 +0100

    finished backend gridfs doesnt work though

---
+++ postModel.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import mongoose from "mongoose";
+
+const postModel = mongoose.Schema({
+	user: String,
+	imgName: String,
+	text: String,
+	avatar: String,
+	timestamp: String,
+});
+
+export default mongoose.model("posts", postModel);
